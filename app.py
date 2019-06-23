c2398936578cf6deeb9202b4e31b634adea6af58
Natasha Hagemeyer <nhagem@gmail.com>
2019-06-23 02:06:10 +0100

    fixed dropped quote

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def graph():
   
   #Pull the requested data into a pandas dataframe
   df = pd.read_csv(StringIO(r.text),sep=',')
-  df['Dates] = pd.to_datetime(df['Date'])
+  df['Date'] = pd.to_datetime(df['Date'])
 
   #df_sub = df[(df['Date'].dt.year==2017) & (df['Date'].dt.month==10)]
 
