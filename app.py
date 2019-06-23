49244b2ce7c0a0e0e99de762205beed28c03d237
Natasha Hagemeyer <nhagem@gmail.com>
2019-06-23 01:57:41 +0100

    messing with bokeh plot

--- app.py
+++ app.py
@@ -9,8 +9,9 @@ from bokeh.embed import components
 app = Flask(__name__)
 
 def get_plot(df):
+  output_file('plot.html')
   p = figure(x_axis_label='Date',y_axis_label='Close')
-  p.line(df['Date'],df['Close'], line_width=2)
+  p = line(df['Date'],df['Close'], line_width=2)
   return(p)
 
 @app.route('/')
